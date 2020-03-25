4a3bbf3494b0b4b7574d6771d065cab7e26a80f1
pratamaizzat <pratama.izzat231298@gmail.com>
2020-03-25 18:50:43 +0700

    initial commit. instalasi Vue-Router dan configurasu Router.js untuk API pada laravel

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Hallo From App.vue</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hallo From Start.vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+};
+</script>
+
+<style></style>
