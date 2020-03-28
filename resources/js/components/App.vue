6749843a930ccfb4356f918b2a2d81bc89c55375
pratamaizzat <pratama.izzat231298@gmail.com>
2020-03-28 15:05:33 +0700

    menyelesaikan pages title dengan state implementasi menggunakan VUEX

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,29 +1,40 @@
 <template>
-    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
-        <Nav />
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+    <Nav />
 
-        <div class="flex overflow-y-hidden flex-1">
-            <Sidebar />
-            <div class="overflow-x-hidden w-2/3">
-                <router-view />
-            </div>
-        </div>
+    <div class="flex overflow-y-hidden flex-1">
+      <Sidebar />
+      <div class="overflow-x-hidden w-2/3">
+        <router-view />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import Nav from "./Nav";
 import Sidebar from "./Sidebar";
 export default {
-    name: "App",
+  name: "App",
 
-    components: {
-        Nav,
-        Sidebar
-    },
+  components: {
+    Nav,
+    Sidebar
+  },
 
-    mounted() {
-        this.$store.dispatch("fetchAuthUser");
+  mounted() {
+    this.$store.dispatch("fetchAuthUser");
+  },
+  // menerima title dari title.js@actions
+  created() {
+    this.$store.dispatch("setPageTitle", this.$route.meta.title); //pada route.js terdapat meta dan title object
+  },
+
+  watch: {
+    $route(to, from) {
+      this.$store.dispatch("setPageTitle", to.meta.title); //pada route.js terdapat meta dan title object
     }
+  }
+  //akhir menerima title
 };
 </script>
---
+++ resources/js/store/modules/title.js
@@ -0,0 +1,35 @@
+const state = {
+    title: 'Welcome'
+};
+
+const getters = {
+    pageTitle: state => {
+        return state.title;
+    }
+};
+
+const actions = {
+    setPageTitle({
+        commit,
+        state
+    }, title) {
+        commit('setTitle', title);
+    }
+};
+
+const mutations = {
+    setTitle(state, title) {
+        state.title = title + ' | Facebook';
+
+        //DOM Title
+        document.title = state.title;
+    }
+};
+
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+}
